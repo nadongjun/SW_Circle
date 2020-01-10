6142196ccee1b84c93e61db7702a1a683f5fe814
nadongjun <40987943+nadongjun@users.noreply.github.com>
2020-01-10 20:37:56 +0900

    Update sketch_dec19a.ino
    문자열 파싱을 위해 ()에서 ^으로 변경

--- sketch_dec19a.ino
+++ sketch_dec19a.ino
@@ -5,12 +5,15 @@ void setup() {
 }
 
 void loop() {
+  
   // put your main code here, to run repeatedly:
   float vibrationSensor = analogRead(A0);
   float soundSensor = analogRead(A1);
   float switchVibrationSensor = analogRead(A2);
   String result = ""; 
-  result = "(" + (String)vibrationSensor + "," + (String)soundSensor +","+ (String)switchVibrationSensor + ")";
+  
+  result = "^" + (String)vibrationSensor + "," + (String)soundSensor +","+ (String)switchVibrationSensor + "^";
   Serial.println(result);
-  delay(2000);
-  }
+  //Serial.println(switchVibrationSensor);
+  //delay(1000);
+ }
